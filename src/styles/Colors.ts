a1365a6d28821a815ed691cf24ac2e5b8140b727
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-06-28 16:38:14 -0500

    feat: Create Colors.ts for global styles

---
+++ src/styles/Colors.ts
@@ -0,0 +1,17 @@
+interface ColorsInterface {
+    primaryColor: string;
+    secondaryColor: string;
+    backgroundColor: string;
+    textColor: string;
+    linkColor: string;
+  }
+  
+  const Colors: ColorsInterface = {
+    primaryColor: "#001F3D",
+    secondaryColor: "#FFDE69",
+    backgroundColor: "#E4E5D5",
+    textColor: "#050522",
+    linkColor: "#FF8A00",
+  };
+  
+  export default Colors;
