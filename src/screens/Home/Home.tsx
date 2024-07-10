24d5551bf3bc247db1e203d8ee88fc877786dc62
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-10 16:16:06 -0500

    feat: Create structure component's styles

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import styles from "./Home.styles";
 
 export default function Home() {
   return (
@@ -10,11 +11,4 @@ export default function Home() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
